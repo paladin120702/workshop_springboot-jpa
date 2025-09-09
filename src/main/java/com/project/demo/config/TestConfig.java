94611294ef9c0548900abfc72b7035f1a60ba6bf
devartur <arturvinicius.estudante.trabalho@gmail.com>
2025-09-09 13:21:01 -0300

    Service layer e componente de registro

--- src/main/java/com/project/demo/config/TestConfig.java
+++ src/main/java/com/project/demo/config/TestConfig.java
@@ -1,7 +1,6 @@
 package com.project.demo.config;
 
 import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
