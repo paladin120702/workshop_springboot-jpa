5a2a8329c6c1b80c92aa1f2c2f61f2bd768ad09a
devartur <arturvinicius.estudante.trabalho@gmail.com>
2025-09-09 11:43:00 -0300

    JPA repository e injeção de dependencia

---
+++ src/main/java/com/project/demo/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.project.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.project.demo.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
