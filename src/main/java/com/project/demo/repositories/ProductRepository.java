73467233a32027c6dad56148b88bc86750a07a74
devartur <arturvinicius.estudante.trabalho@gmail.com>
2025-09-15 14:10:06 -0300

    Implementando Product

---
+++ src/main/java/com/project/demo/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.project.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.project.demo.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
